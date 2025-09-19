c17273b89d71010f54f1d394b705ebd0724770eb
Hobin Han <hobin1019@gmail.com>
2025-09-19 15:57:12 +0900

    Add files via upload

---
+++ SwiftConcurrencyLab/SwiftConcurrencyLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyLabApp.swift
+//  SwiftConcurrencyLab
+//
+//  Created by Hobin Han on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tests/SwiftConcurrencyLab.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftConcurrencyLab.swift
+//  ImageLoaderTests
+//
+//  Created by Hobin Han on 9/19/25.
+//
+
+import Testing
+
+struct ImageLoaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
