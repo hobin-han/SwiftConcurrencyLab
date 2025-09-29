47306ebfa7949ba559d9d621fe24f1366fce5e95
hobin-han <hobin1019@gmail.com>
2025-09-29 11:07:59 +0900

    chore(target): change target name

---
+++ SwiftConcurrencyLabTests/SwiftConcurrencyLabTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftConcurrencyLabTests.swift
+//  SwiftConcurrencyLabTests
+//
+//  Created by bamiboo.han on 9/29/25.
+//
+
+import Testing
+
+struct SwiftConcurrencyLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
